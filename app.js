ff77f012986bd360b2344954e6d1d59c48e7f5f6
MeganSmith <46550038+meganmsmith58@users.noreply.github.com>
2019-06-20 10:39:52 -0600

    added new_users_to_main_page

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'pug');
 app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-    res.render('index', {message: users.users});
+    res.render('index', {message: users.usersArray});
 });
 app.get('/form', (req, res) => {
     res.render('form');
@@ -24,30 +24,30 @@ app.get('/edit', (req, res) => {
 });
 
 app.post('/create', (req, res) => {
-    console.log(req.body);
-    saveUser(req.body);
+    let user = {};
+    user.username = req.body.username;
+    user.name = req.body.name;
+    user.email = req.body.email;
+    user.age = req.body.age;
+    console.log(user);
+    // saveUser(req.body);
     // res.end(`${req.body.username} ${req.body.name} ${req.body.email} ${req.body.age}`);
-    res.redirect('/');
-});
-app.post('/update', (req, res) => {
-    saveUser(res);
-    // res.end(`${req.body.username} ${req.body.name} ${req.body.email} ${req.body.age}`);
-});
-
-saveUser = (newUser) => {
-    // const userInfo = JSON.stringify(newUser);
-    // fs.writeFileSync('users.json', userInfo, (err) => {
-    //     if (err) throw err;
-    // } );
     fs.readFile('users.json', (err, data) => {
         let json = JSON.parse(data);
-        json.users.push(newUser);
-        // console.log(json);
+        json.usersArray.push(user);
+        // console.log(json.usersArray);
+        users = json;
         fs.writeFile('users.json', JSON.stringify(json), err => {
             if (err) throw err;
-        })
-    })
-};
+            res.redirect('/');
+        });
+    });
+
+});
+app.post('/update', (req, res) => {
+    // saveUser(res);
+    // res.end(`${req.body.username} ${req.body.name} ${req.body.email} ${req.body.age}`);
+});
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
